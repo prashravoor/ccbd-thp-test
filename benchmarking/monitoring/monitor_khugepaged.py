6339f023febfe3e84a406ebef75997be72a87230
Prashanth Ravoor <prash.ravoor125@gmail.com>
2020-01-06 10:18:38 +0530

    Monitoring CompactD and SwapD as well

--- benchmarking/monitoring/monitor_khugepaged.py
+++ benchmarking/monitoring/monitor_khugepaged.py
@@ -20,31 +20,46 @@ def get_huge_page_per(pid):
 
 
 thp_proc_name='khugepaged'
+kswapd_names = list(map(lambda x: 'kswapd{}'.format(x), range(8)))
+kcompactd_names = list(map(lambda x: 'kcompactd{}'.format(x), range(8)))
+
 mdb_proc_name='/usr/bin/mongod'
-thp_pid = int(check_output(['pidof', thp_proc_name]))
+thp_pids = [int(check_output(['pidof', thp_proc_name]))]
 mdb_pid = int(check_output(['pidof', mdb_proc_name]))
+
+for i in range(8):
+    thp_pids.append(int(check_output(['pidof', kswapd_names[i]])))
+    thp_pids.append(int(check_output(['pidof', kcompactd_names[i]])))
 
 # log('PID of khugepaged process is {}'.format(pid))
 # log('')
 # log('Starting to monitor {}'.format(pid))
 log('Memory Used (%) Overall,CPU (%) Overall, Memory Used (%) MongoDB, CPU (%) MongoDB, Memory Used (%) khugepaged, CPU (%) khugepaged,Process HugePages (%)')
 
-thp_proc = psutil.Process(thp_pid)
+thp_procs = list(map(lambda x: psutil.Process(x), thp_pids))
 mdb_proc = psutil.Process(mdb_pid)
 
 num_cpus = psutil.cpu_count()
+# num_cpus = 12
 
 psutil.cpu_percent()
 mdb_proc.cpu_percent()
-thp_proc.cpu_percent()
+for thp_proc in thp_procs:
+    thp_proc.cpu_percent()
 sys_mem_kb = (psutil.virtual_memory().total) / 1024
 
 while True:
     time.sleep(1)
+    thp_cpu = 0
+    thp_mem = 0
+    for thp_proc in thp_procs:
+        thp_cpu += thp_proc.cpu_percent()
+        thp_mem += thp_proc.memory_percent()
+
     log('{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}'.format(
             100 * (psutil.virtual_memory().used/psutil.virtual_memory().total), (psutil.cpu_percent()),
             mdb_proc.memory_percent(), mdb_proc.cpu_percent()/num_cpus, # Normalize to 100%
-            thp_proc.memory_percent(), thp_proc.cpu_percent()/num_cpus, # Normalize to 100%
+            thp_mem, thp_cpu/num_cpus, # Normalize to 100%
             get_huge_page_per(mdb_pid)
         )
     ) 
